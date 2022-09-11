9e18a883130a1bcc29cd9c01d6510b6049e314a3
Ahmed Semih Erkan <ahmedsemiherkan@gmail.com>
2022-09-11 15:36:23 +0300

    Question area created

---
+++ src/components/QuestionArea.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const QuestionArea = () => {
+  return (
+    <div className='question-area'>
+        <div className="question">
+        Question Text
+        </div>
+        <div className="answers">
+            <div className="answer">
+                Answer 1
+            </div>
+            <div className="answer">
+                Answer 2
+            </div>
+            <div className="answer">
+                Answer 3
+            </div>
+            <div className="answer">
+                Answer 4
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default QuestionArea;
--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 
 import MoneyTree from "../components/MoneyTree";
+import QuestionArea from "../components/QuestionArea";
+import Timer from "../components/Timer";
 
 const Quiz = () => {
   return (
     <div className="quiz-main">
-      <div className="question-side">LEft</div>
+      <div className="question-side">
+        <Timer/>
+        <QuestionArea/>
+      </div>
       <MoneyTree />
     </div>
   );
