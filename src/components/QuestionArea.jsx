cc421bfc155ef495c8a13361b146a218bd28582a
Ahmed Semih Erkan <ahmedsemiherkan@gmail.com>
2022-09-12 15:19:49 +0300

    Quiz logic created

--- src/components/QuestionArea.jsx
+++ src/components/QuestionArea.jsx
@@ -1,27 +1,50 @@
-import React from 'react';
+import React, { useState } from "react";
 
-const QuestionArea = () => {
+const QuestionArea = ({ questions, currentQuestion, setCurrentQuestion, setStop }) => {
+  const [currentClass, setCurrentClass] = useState("answer");
+  const [selectedAnswer, setSelectedAnswer] = useState(null);
+
+  const handleClick = (answer) => {
+    if (selectedAnswer === null) {
+      setSelectedAnswer(answer);
+      setCurrentClass("answer active");
+
+      setTimeout(()=>{
+        setCurrentClass(answer.isCorrect ? "answer correct" : "answer wrong");
+      },3000)
+
+      setTimeout(()=>{
+        setSelectedAnswer(null)
+        if (answer.isCorrect) {
+          setCurrentQuestion((prev) => prev + 1);
+        } else {
+          setStop(true);
+        }
+      },6000);
+    }
+  };
+
   return (
-    <div className='question-area'>
-        <div className="question">
-        Question Text
-        </div>
-        <div className="answers">
-            <div className="answer">
-                Answer 1
-            </div>
-            <div className="answer">
-                Answer 2
-            </div>
-            <div className="answer">
-                Answer 3
-            </div>
-            <div className="answer">
-                Answer 4
-            </div>
-        </div>
+    <div className="question-area">
+      <div className="question">
+        {questions && questions[currentQuestion].question}
+      </div>
+      <div className="answers">
+        {questions &&
+          questions[currentQuestion].answers.map((answer, index) => {
+            return (
+              <div
+                key={index}
+                onClick={() => handleClick(answer)}
+                className={selectedAnswer === answer ? currentClass : "answer"}
+              >
+                {answer.text}
+              </div>
+            );
+          })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default QuestionArea;
--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -1,17 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import MoneyTree from "../components/MoneyTree";
 import QuestionArea from "../components/QuestionArea";
+import Result from "../components/Result";
 import Timer from "../components/Timer";
 
+import { Easy, Medium, Hard } from "../utils/questions";
+import questionSelector from "../utils/questionSelector";
+
 const Quiz = () => {
+  const [currentQuestion, setCurrentQuestion] = useState(1);
+  const [questions, setQuestions] = useState([]);
+  const [stop, setStop] = useState(false);
+
+  useEffect(() => {
+    setQuestions(questionSelector(Easy, Medium, Hard));
+  }, []);
+
   return (
     <div className="quiz-main">
       <div className="question-side">
-        <Timer/>
-        <QuestionArea/>
+        {
+          stop 
+          ? 
+          <Result />
+          :
+          <>
+          <Timer />
+          <QuestionArea
+            questions={questions.length > 0 && questions}
+            currentQuestion={currentQuestion - 1}
+            setCurrentQuestion={setCurrentQuestion}
+            setStop={setStop}
+          />
+          </>
+        }
       </div>
-      <MoneyTree />
+      <MoneyTree currentQuestion={currentQuestion} stop={stop} />
     </div>
   );
 };
